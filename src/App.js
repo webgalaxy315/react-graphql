be5b11be83fa9ca86dfcf447324d0ae959b7b2a2
ZengGang315 <zoelasa1111@gmail.com>
2022-10-08 16:58:46 +0900

    dep: Fetch data with useQuery

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import logo from './logo.svg';
+import { useQuery, gql } from '@apollo/client';
+
 import './App.css';
+
+const GET_LOCATIONS = gql`
+  query GetLocations {
+    locations {
+      id
+      name
+      description
+      photo
+    }
+  }
+`;
+
+const DisplayLocations = () => {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.locations.map(({ id, name, description, photo }) => (
+    <div key={id}>
+      <h3>{name}</h3>
+      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
+      <br />
+      <b>About this location:</b>
+      <p>{description}</p>
+      <br />
+    </div>
+  ));
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <DisplayLocations />
     </div>
   );
 }
