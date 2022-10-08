f46cdab5a03a3628fc8fc23c7bf7fa0b85ad1e41
ZengGang315 <zoelasa1111@gmail.com>
2022-10-08 16:50:54 +0900

    dep: Connect your client to React

--- src/index.js
+++ src/index.js
@@ -29,9 +29,9 @@ client
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
